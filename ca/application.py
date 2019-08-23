87c80e7f50e55bd5a4ee5913ddcd53eb18b7308f
jiho <search5@gmail.com>
2019-08-23 21:16:06 +0900

    CA 인증기관 만들기 성공

--- ca/application.py
+++ ca/application.py
@@ -1,8 +1,11 @@
 import os
+import sys
 import uuid
 from pathlib import Path
+from tempfile import TemporaryFile, NamedTemporaryFile
 
 import paginate as paginate
+import pexpect
 from flask import Flask, render_template, request, jsonify, url_for
 from paginate_sqlalchemy import SqlalchemyOrmWrapper
 from sqlalchemy import desc
@@ -98,25 +101,82 @@ def ca_add_post():
                 new_ca_root = Path(os.environ["CA_ROOTS"]) / ca_record.catop
                 new_ca_root.mkdir(parents=True, exist_ok=True)
 
+                # create the directory hierarchy
+                for item in ("certs", "crl", "newcerts", "private"):
+                    Path(new_ca_root / item).mkdir(parents=True, exist_ok=True)
+
+                Path(new_ca_root / "index.txt").write_text('')
+                Path(new_ca_root / "crlnumber").write_text('01\n')
+
+                ca_root_resolve = str(new_ca_root.resolve())
                 caconfig = p.read_text()
-                caconfig = caconfig.replace("./demoCA", str(new_ca_root.resolve()))
+                caconfig = caconfig.replace("./demoCA", ca_root_resolve)
                 ca_record.caconfig = caconfig
 
-                # Enter PEM pass phrase: <password>⏎
-                # Verifying - Enter PEM pass phrase: <password>
+                print("Making CA certificate ...\n")
 
-                # Country Name (2 letter code) [AU]:KR⏎
-                # State or Province Name (full name) [Some-State]:⏎
-                # Locality Name (eg, city) []:Seoul⏎
-                # Organization Name (eg, company) [Internet Widgits Pty Ltd]:⏎
-                # Organizational Unit Name (eg, section) []:⏎
-                # Common Name (e.g. server FQDN or YOUR name) []:ca.insignal.co.kr⏎
-                # Email Address []: ⏎
-                #
-                # Please enter the following 'extra' attributes
-                # to be sent with your certificate request
-                # A challenge password []:⏎
-                # An optional company name []:⏎
+                ssl_cnf = NamedTemporaryFile("w+", delete=False)
+                ssl_cnf.write(caconfig)
+                ssl_cnf.seek(0)
+
+                REQ = "openssl req -config {ssl_cnf}".format(ssl_cnf=ssl_cnf.name)
+
+                # TODO: extra options... (1 args)
+                RET1 = "{REQ} -new -keyout {CAKEY} -out {CAREQ}".format(
+                    REQ=REQ,
+                    CAKEY=new_ca_root / "private" / "cakey.pem",
+                    CAREQ=new_ca_root / "careq.pem"
+                )
+
+                ssl_req = pexpect.spawn(RET1, encoding='utf-8')
+                ssl_req.expect('Enter PEM pass phrase:')
+                ssl_req.sendline(ca_record.capass)
+                ssl_req.expect('Verifying - Enter PEM pass phrase:')
+                ssl_req.sendline(ca_record.capass)
+
+                ssl_req.expect('Country Name.*:')
+                ssl_req.sendline(ca_record.country_name)
+                ssl_req.expect('State or Province Name.*:')
+                ssl_req.sendline(ca_record.province_name)
+                ssl_req.expect('Locality Name.*:')
+                ssl_req.sendline(ca_record.locality_name)
+                ssl_req.expect('Organization Name.*:')
+                ssl_req.sendline(ca_record.organization_name)
+                ssl_req.expect('Organizational Unit Name.*:')
+                ssl_req.sendline(ca_record.organizational_unit_name)
+                ssl_req.expect('Common Name.*:')
+                ssl_req.sendline(ca_record.common_name)
+                ssl_req.expect('Email Address.*:')
+                ssl_req.sendline(ca_record.email_address)
+                ssl_req.expect('A challenge password.*:')
+                ssl_req.sendline(".")
+                ssl_req.expect('An optional company name.*:')
+                ssl_req.sendline(".")
+                ssl_req.expect(pexpect.EOF)
+                ssl_req.wait()
+
+                CA_CMD = "openssl ca -config {ssl_cnf}".format(ssl_cnf=ssl_cnf.name)
+
+                RET2 = ("{CA} -create_serial"
+                        " -out {CACERT} -days {CADAYS} -batch"
+                        " -keyfile {CAKEY} -selfsign"
+                        " -extensions v3_ca"
+                        " -infiles {CAREQ}").format(
+                    CA=CA_CMD,
+                    CACERT=new_ca_root / "cacert.pem",
+                    CADAYS=ca_record.cadays,
+                    CAKEY=new_ca_root / "private" / "cakey.pem",
+                    CAREQ=new_ca_root / "careq.pem"
+                )
+
+
+                ssl_ca = pexpect.spawn(RET2, encoding='utf-8')
+                ssl_ca.expect('Enter pass phrase for.*:')
+                ssl_ca.sendline(ca_record.capass)
+                ssl_ca.expect(pexpect.EOF)
+                ssl_ca.wait()
+
+                print("CA certificate is in {CACERT}".format(CACERT=new_ca_root / "cacert.pem"))
 
         db_session.add(ca_record)
         db_session.commit()
